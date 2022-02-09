2e875ed5f39ca23d04cd90225c5b805077d48966
Marco Magno <mmagno@student.ethz.ch>
2022-02-09 19:06:11 +0100

    fixed bugs

--- giorgia_experiments.bat
+++ giorgia_experiments.bat
@@ -1,10 +1,10 @@
 python main.py --numpy_seed 1 --torch_seed 1 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
-python main.py --numpy_seed 2 --torch_seed 2 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds [25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 3 --torch_seed 3 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 4 --torch_seed 4 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 5 --torch_seed 5 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 6 --torch_seed 6 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 7 --torch_seed 7 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 8 --torch_seed 8 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 9 --torch_seed 9 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
-python main.py --numpy_seed 10 --torch_seed 10 --size 10 --n_agents 10 --weights [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5] --thresholds=[25,25,25,25,25,25,25,25,25,25]
+python main.py --numpy_seed 2 --torch_seed 2 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 3 --torch_seed 3 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 4 --torch_seed 4 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 5 --torch_seed 5 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 6 --torch_seed 6 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 7 --torch_seed 7 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 8 --torch_seed 8 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 9 --torch_seed 9 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
+python main.py --numpy_seed 10 --torch_seed 10 --size 10 --n_agents 10 --weights 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 --thresholds 25 25 25 25 25 25 25 25 25 25
