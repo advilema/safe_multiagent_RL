aeff743871b95acb8fe90eb4181c0836e265a2bd
Marco Magno <mmagno@student.ethz.ch>
2022-04-29 11:49:59 +0200

    Congestion environment updates

--- cli_parse.py
+++ cli_parse.py
@@ -6,7 +6,7 @@ def cli():
     parser.add_argument('--unconstrained', type=bool, default=False)
     parser.add_argument('--numpy_seed', type=int, default=0)
     parser.add_argument('--torch_seed', type=int, default=0)
-    parser.add_argument('--environment', type=str, default='ExploreDiscrete')
+    parser.add_argument('--environment', type=str, default='Congestion')
     parser.add_argument('--n_meta_agent_learning_cycles', type=int, default=1)
     parser.add_argument('--n_agents_learning_cycles', type=int, default=50)
     parser.add_argument('--batch_size', type=int, default=50)
