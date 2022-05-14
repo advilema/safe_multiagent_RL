2604dd18f93a641ebba09564255da112d4ca0ba8
Marco Magno <mmagno@student.ethz.ch>
2022-05-14 16:19:21 +0200

    updated run

--- marco_experiments.bat
+++ marco_experiments.bat
@@ -1,8 +1,3 @@
-python main.py --numpy_seed 1 --torch_seed 1 --unconstrained
-python main.py --numpy_seed 2 --torch_seed 2 --unconstrained
-python main.py --numpy_seed 3 --torch_seed 3 --unconstrained
-python main.py --numpy_seed 4 --torch_seed 4 --unconstrained
-python main.py --numpy_seed 5 --torch_seed 5 --unconstrained
 python main.py --numpy_seed 6 --torch_seed 6 --unconstrained
 python main.py --numpy_seed 7 --torch_seed 7 --unconstrained
 python main.py --numpy_seed 8 --torch_seed 8 --unconstrained
