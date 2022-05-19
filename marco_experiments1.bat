78f2354f175b249bbf4e02e54a650b23a7cd2ad9
Marco Magno <mmagno@student.ethz.ch>
2022-05-19 04:52:58 +0200

    added comparing code

--- marco_experiments1.bat
+++ marco_experiments1.bat
@@ -1,4 +1,4 @@
-python main.py --numpy_seed 0 --torch_seed 0 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.003 --meta_lr 0.002 --thresholds 2 --environment Space
+python main.py --numpy_seed 0 --torch_seed 0 --n_meta_agent_learning_cycles 100 --n_agents_learning_cycles 30 --lr 0.003 --meta_lr 0.01 --thresholds 0.5 --environment Space --shuffle --lambda_0 1
 python main.py --numpy_seed 1 --torch_seed 1 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.003 --meta_lr 0.002 --thresholds 2 --environment Space
 python main.py --numpy_seed 2 --torch_seed 2 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.003 --meta_lr 0.002 --thresholds 2 --environment Space
 python main.py --numpy_seed 3 --torch_seed 3 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.003 --meta_lr 0.002 --thresholds 2 --environment Space
