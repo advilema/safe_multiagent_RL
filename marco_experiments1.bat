5b5e0735c8c4207780c4e4862f99b877e970aba9
Marco Magno <mmagno@student.ethz.ch>
2022-05-19 06:34:34 +0200

    updated runs

--- marco_experiments1.bat
+++ marco_experiments1.bat
@@ -1,5 +1,5 @@
-python CMIX/compare.py --numpy_seed 0 --torch_seed 0 --training-epochs 100000 --max_env_t 10 --environment Congestion --threshold 1.5 --shuffle
-python CMIX/compare.py --numpy_seed 1 --torch_seed 1 --training-epochs 100000 --max_env_t 10 --environment Congestion --threshold 1.5 --shuffle
-python CMIX/compare.py --numpy_seed 2 --torch_seed 2 --training-epochs 100000 --max_env_t 10 --environment Congestion --threshold 1.5 --shuffle
-python CMIX/compare.py --numpy_seed 3 --torch_seed 3 --training-epochs 100000 --max_env_t 10 --environment Congestion --threshold 1.5 --shuffle
-python CMIX/compare.py --numpy_seed 4 --torch_seed 4 --training-epochs 100000 --max_env_t 10 --environment Congestion --threshold 1.5 --shuffle
+python CMIX/compare.py --numpy_seed 0 --torch_seed 0 --training-epochs 100000 --max-env-t 10 --environment Congestion --threshold 1.5 --shuffle
+python CMIX/compare.py --numpy_seed 1 --torch_seed 1 --training-epochs 100000 --max-env-t 10 --environment Congestion --threshold 1.5 --shuffle
+python CMIX/compare.py --numpy_seed 2 --torch_seed 2 --training-epochs 100000 --max-env-t 10 --environment Congestion --threshold 1.5 --shuffle
+python CMIX/compare.py --numpy_seed 3 --torch_seed 3 --training-epochs 100000 --max-env-t 10 --environment Congestion --threshold 1.5 --shuffle
+python CMIX/compare.py --numpy_seed 4 --torch_seed 4 --training-epochs 100000 --max-env-t 10 --environment Congestion --threshold 1.5 --shuffle
