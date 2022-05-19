0f2273e5ba6250318f6935969e6ff07e48da387f
Marco Magno <mmagno@student.ethz.ch>
2022-05-19 06:16:59 +0200

    updated runs

--- pragnya_experiments1.bat
+++ pragnya_experiments1.bat
@@ -1,1 +1,5 @@
-python compare/compare.py --numpy_seed 0 --torch_seed 0 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --thresholds 15 15 15 --environment ExploreDiscretized
+python main.py --numpy_seed 0 --torch_seed 0 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 1 --torch_seed 1 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 2 --torch_seed 2 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 3 --torch_seed 3 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 4 --torch_seed 4 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
--- pragnya_experiments2.bat
+++ pragnya_experiments2.bat
@@ -1,5 +1,5 @@
-python main.py --numpy_seed 5 --torch_seed 5 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6
-python main.py --numpy_seed 6 --torch_seed 6 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --thresholds 15 15 15 --environment ExploreContinuous
-python main.py --numpy_seed 7 --torch_seed 7 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --thresholds 15 15 15 --environment ExploreContinuous
-python main.py --numpy_seed 8 --torch_seed 8 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --thresholds 15 15 15 --environment ExploreContinuous
-python main.py --numpy_seed 9 --torch_seed 9 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --thresholds 15 15 15 --environment ExploreContinuous
+python main.py --numpy_seed 5 --torch_seed 5 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 6 --torch_seed 6 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 7 --torch_seed 7 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 8 --torch_seed 8 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
+python main.py --numpy_seed 9 --torch_seed 9 --n_meta_agent_learning_cycles 40 --n_agents_learning_cycles 30 --lr 0.0015 --meta_lr 0.003 --thresholds 15 15 15 --environment ExploreContinuous --coarseness 6 --shuffle
