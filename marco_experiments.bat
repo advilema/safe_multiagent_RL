80623328a03752ba56e4c89ab88ae84748bcefd1
Marco Magno <mmagno@student.ethz.ch>
2022-06-08 15:08:59 +0200

    eliminated runs files

--- marco_experiments.bat
+++
@@ -1,1 +0,0 @@
-python main.py --numpy_seed 0 --torch_seed 0 --n_meta_agent_learning_cycles 200 --n_agents_learning_cycles 10 --n_agents 3 --actor_lr 0.0015 --critic_lr 0.0005 --meta_lr 0.002 --size 3 --noise 0.1 --thresholds 1.5 --environment Space --algo ac --max_t 10 --gamma 0.9
